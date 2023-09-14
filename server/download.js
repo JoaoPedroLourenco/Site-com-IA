5ba5ec42585cb572e6aa03bbeacfb981f335433c
João Pedro Lourenço dos Santos <105937488+JoaoPedroLourenco@users.noreply.github.com>
2023-09-13 23:43:40 -0300

    site com ia finalizado

---
+++ server/download.js
@@ -0,0 +1,24 @@
+import ytdl from 'ytdl-core'
+import fs from 'fs'
+import { rejects } from 'assert'
+
+export const download = (videoId) => new Promise((resolve, reject) =>  {
+  const videoURL = "https://www.youtube.com/shorts/" + videoId
+  console.log("Realizando o download do vídeo:", videoId)
+
+  ytdl(videoURL, {quality: "lowestaudio", filter: "audioonly"}).on("info",
+   (info) => {
+    const seconds = info.formats[0].approxDurationMs / 1000
+    
+    if(seconds > 60){
+      throw new Error("Duração maior que 60 segundos.")
+    }
+  }).on("end", () => {
+    console.log("Download do vídeo finalizado.")
+    resolve()
+  })
+  .on("error", (error) => {
+    console.log("não foi possível concluir o download do vídeo. Detalhes do erro: ", error)
+    reject(error)
+  }).pipe(fs.createWriteStream('./tmp/audio.mp4'))
+})
